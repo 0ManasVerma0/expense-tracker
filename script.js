f57d579d5cb6b99b6aaf096e33062abb6d96e349
Manas verma <manasverma1357@gmail.com>
2025-05-05 17:28:23 +0530

    Select all dom elements & localstorage setup

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const expenseForm = document.getElementById("expense-form");
+    const expenseNameInput = document.getElementById("expense-name");
+    const expenseAmountInput = document.getElementById("expense-amount");
+    const expenseList = document.getElementById("expense-list");
+    const totalAmountDisplay = document.getElementById("total-amount");
+
+    let expenses = JSON.parse(localStorage.getItem("expense")) || [];
+    let totalAmount = calculateTotal();
+
+    function saveExpenses(){
+        localStorage.setItem("expense", JSON.stringify(expenses))
+    }
+})
